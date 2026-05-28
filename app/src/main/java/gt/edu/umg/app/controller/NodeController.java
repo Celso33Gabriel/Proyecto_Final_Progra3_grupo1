67a5a2b44efd954403a96f3e1e1af660f26d12ca
Luis Emilio <milo12flores@gmail.com>
2026-05-28 15:27:16 -0600

    fix(B): agrega nombres en @PathVariable y @RequestParam del NodeController

--- app/src/main/java/gt/edu/umg/app/controller/NodeController.java
+++ app/src/main/java/gt/edu/umg/app/controller/NodeController.java
@@ -16,35 +16,33 @@ public class NodeController {
     @PostMapping("/root")
     public void createRoot(@RequestParam("id") String id,
                            @RequestParam("value") String value) {
-
         repository.createRoot(id, value);
     }
 
     @PostMapping("/{parentId}/children")
-    public void addChild(@PathVariable String parentId,
+    public void addChild(@PathVariable("parentId") String parentId,
                          @RequestParam("id") String id,
                          @RequestParam("value") String value) {
-
         repository.addChild(parentId, id, value);
     }
 
     @GetMapping("/{nodeId}/path")
-    public Object getPath(@PathVariable String nodeId) {
+    public Object getPath(@PathVariable("nodeId") String nodeId) {
         return repository.getPathFromRoot(nodeId);
     }
 
     @GetMapping("/{nodeId}/depth")
-    public int getDepth(@PathVariable String nodeId) {
+    public int getDepth(@PathVariable("nodeId") String nodeId) {
         return repository.getDepth(nodeId);
     }
 
     @GetMapping("/{nodeId}/ancestors")
-    public Object getAncestors(@PathVariable String nodeId) {
+    public Object getAncestors(@PathVariable("nodeId") String nodeId) {
         return repository.getAncestors(nodeId);
     }
 
     @GetMapping("/{nodeId}/subtree")
-    public Object getSubtree(@PathVariable String nodeId) {
+    public Object getSubtree(@PathVariable("nodeId") String nodeId) {
         return repository.getSubtree(nodeId);
     }
 }
