8eae4053ba5d12ecf8354c365059f30a1d6c02b2
Mario-Tereta <mteretas@miumg.edu.gt>
2026-05-30 01:57:30 -0600

    Feat C: Integracion de MOngo

--- app/src/main/java/gt/edu/umg/app/repository/SpringDataTreeRepository.java
+++ app/src/main/java/gt/edu/umg/app/repository/SpringDataTreeRepository.java
@@ -1,10 +1,16 @@
 package gt.edu.umg.app.repository;
 
 import gt.edu.umg.app.model.TreeNode;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpringDataTreeRepository extends JpaRepository<TreeNode, String> {
-    // JpaRepository ya incluye por defecto los métodos .save(), .findAll(), .delete(), etc.
+@ConditionalOnProperty(
+        name = "app.storage",
+        havingValue = "postgres"
+)
+public interface SpringDataTreeRepository
+        extends JpaRepository<TreeNode, String> {
+
 }
